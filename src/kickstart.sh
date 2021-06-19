e4050ba12d7080fbb871b511d78569f2f96e0a7e
ellisway <ellism3zew@gmail.com>
2021-06-19 20:40:37 +0100

    Update kickstart.sh
    add some initial swap test logic

--- src/kickstart.sh
+++ src/kickstart.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# configure system to use networkradius.com binary packages these are newer than those supplied by ubuntu 
+# configure system to use networkradius.com binary packages these are newer than those supplied by ubuntu
 
 if lsb_release -d | awk -F"\t" '{print $2}' | grep -q 20.04
                 then
                 echo "found ubuntu 20.04"
-                echo "adding (deb https://packages.networkradius.com/releases/ubuntu-focal focal main) to (/etc/apt/sources.list)" 
-                echo 'deb https://packages.networkradius.com/releases/ubuntu-focal focal main' >> /etc/apt/sources.list 
+                echo "adding (deb https://packages.networkradius.com/releases/ubuntu-focal focal main) to (/etc/apt/sources.list)"
+                echo 'deb https://packages.networkradius.com/releases/ubuntu-focal focal main' >> /etc/apt/sources.list
         elif lsb_release -d | awk -F"\t" '{print $2}' | grep -q 18.04
                 then
                 echo "found ubuntu 18.04"
-                echo "adding ( https://packages.networkradius.com/releases/ubuntu-bionic bionic main) to (/etc/apt/sources.list)" 
-                echo 'deb https://packages.networkradius.com/releases/ubuntu-bionic bionic main' >> /etc/apt/sources.list 
+                echo "adding ( https://packages.networkradius.com/releases/ubuntu-bionic bionic main) to (/etc/apt/sources.list)"
+                echo 'deb https://packages.networkradius.com/releases/ubuntu-bionic bionic main' >> /etc/apt/sources.list
 fi
 
-# import networkradius.com pgp key 
+# import networkradius.com pgp key
 
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key 0x41382202
 
@@ -26,11 +26,26 @@ sudo apt-get install --yes freeradius freeradius-common freeradius-utils freerad
 
 ### we should test for an existing swap device or file before we go ahead and try to create one  ###
 
-/usr/bin/fallocate -l 4G /swapfile
-/bin/chmod 600 /swapfile
-/sbin/mkswap /swapfile
-/sbin/swapon /swapfile
-echo '/swapfile none swap sw 0 0' >> /etc/fstab
+if cat /etc/fstab | grep -q swap
+        then
+                echo "swap was found in fstab"
+                if free | awk '/^Swap:/ {exit !$2}'
+                        then
+                                echo "swap is enabled"
+                        else
+                                echo "swap was not enabled we will enable it now"
+                                swapon -a
+                fi
+        else
+            echo "no swap found in fstab we will now create and enable a swapfile"
+            /usr/bin/fallocate -l 4G /swapfile
+            /bin/chmod 600 /swapfile
+            /sbin/mkswap /swapfile
+            /sbin/swapon /swapfile
+            echo '/swapfile none swap sw 0 0' >> /etc/fstab
+fi
+
+
 
 /sbin/sysctl vm.swappiness=10
 echo 'vm.swappiness=10' >> /etc/sysctl.conf
@@ -44,3 +59,4 @@ echo '# PUT YOUR MYSQL PASSWORD YOU JUST ENTERED BELOW, THEN PRESS CTRL+X and Y 
 echo 'MYSQL_PASSWORD=changeme' >> ~/freeradius3-genie/.env
 
 nano ~/freeradius3-genie/.env
+# eof
