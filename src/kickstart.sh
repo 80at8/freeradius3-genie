2cb17a0e8008997c16ccf1f3e7f8bb413c1c0f92
ellisway <ellism3zew@gmail.com>
2021-06-20 07:25:41 +0100

    Update kickstart.sh

--- src/kickstart.sh
+++ src/kickstart.sh
@@ -2,16 +2,34 @@
 
 # configure system to use networkradius.com binary packages these are newer than those supplied by ubuntu
 
-if lsb_release -d | awk -F"\t" '{print $2}' | grep -q 20.04
+# determine distribution and version more logic needed so we dont create multiple entries if the script is re run 
+
+if lsb_release -d | awk -F"\t" '{print $2}' | grep -q Ubuntu
+    then
+            if lsb_release -d | awk -F"\t" '{print $2}' | grep -q 20.04
                 then
-                echo "found ubuntu 20.04"
-                echo "adding (deb https://packages.networkradius.com/releases/ubuntu-focal focal main) to (/etc/apt/sources.list)"
-                echo 'deb https://packages.networkradius.com/releases/ubuntu-focal focal main' >> /etc/apt/sources.list
-        elif lsb_release -d | awk -F"\t" '{print $2}' | grep -q 18.04
+                    echo "found ubuntu 20.04"
+                    echo "adding (deb https://packages.networkradius.com/releases/ubuntu-focal focal main) to (/etc/apt/sources.list)"
+                    echo 'deb https://packages.networkradius.com/releases/ubuntu-focal focal main' >> /etc/apt/sources.list
+            elif lsb_release -d | awk -F"\t" '{print $2}' | grep -q 18.04
                 then
-                echo "found ubuntu 18.04"
-                echo "adding ( https://packages.networkradius.com/releases/ubuntu-bionic bionic main) to (/etc/apt/sources.list)"
-                echo 'deb https://packages.networkradius.com/releases/ubuntu-bionic bionic main' >> /etc/apt/sources.list
+                    echo "found ubuntu 18.04"
+                    echo "adding ( https://packages.networkradius.com/releases/ubuntu-bionic bionic main) to (/etc/apt/sources.list)"
+                    echo 'deb https://packages.networkradius.com/releases/ubuntu-bionic bionic main' >> /etc/apt/sources.list
+            fi
+elif lsb_release -d | awk -F"\t" '{print $2}' | grep -q Debian
+    then
+            if lsb_release -d | awk -F"\t" '{print $2}' | grep -q stretch
+                then
+                    echo "found Debian stretch"
+                    echo "adding (deb https://packages.networkradius.com/releases/debian-stretch stretch main) to (/etc/apt/sources.list)"
+                    echo 'deb https://packages.networkradius.com/releases/debian-stretch stretch main' >> /etc/apt/sources.list
+            elif lsb_release -d | awk -F"\t" '{print $2}' | grep -q buster
+                then
+                    echo "found Debian buster"
+                    echo "adding (deb https://packages.networkradius.com/releases/debian-buster buster main) to (/etc/apt/sources.list)"
+                    echo 'deb https://packages.networkradius.com/releases/debian-buster buster main' >> /etc/apt/sources.list
+            fi
 fi
 
 # import networkradius.com pgp key
@@ -24,7 +42,7 @@ sudo apt-get install --yes php-cli php-mbstring php-mysql unzip
 sudo apt-get install --yes mariadb-server mariadb-client
 sudo apt-get install --yes freeradius freeradius-common freeradius-utils freeradius-mysql
 
-### we should test for an existing swap device or file before we go ahead and try to create one  ###
+### test for an existing swap device or file before we create one  ###
 
 if cat /etc/fstab | grep -q swap
         then
@@ -45,15 +63,13 @@ if cat /etc/fstab | grep -q swap
             echo '/swapfile none swap sw 0 0' >> /etc/fstab
 fi
 
-
-
 /sbin/sysctl vm.swappiness=10
 echo 'vm.swappiness=10' >> /etc/sysctl.conf
 /sbin/sysctl vm.vfs_cache_pressure=50
 echo 'vm.vfs_cache_pressure=50' >> /etc/sysctl.c
-
+echo ""
 echo "if this is your first run it is now time to secure the mysql installation this will allow you to create the root mysql passowrd needed later on "
-
+echo ""
 sudo /usr/bin/mysql_secure_installation
 echo '# PUT YOUR MYSQL PASSWORD YOU JUST ENTERED BELOW, THEN PRESS CTRL+X and Y to SAVE CHANGES' >> ~/freeradius3-genie/.env
 echo 'MYSQL_PASSWORD=changeme' >> ~/freeradius3-genie/.env
